9bd258bc4901d59559f6371c19cb279ee10c5f77
Famas1219 <2501894@learn.vsb.bc.ca>
2021-11-10 15:06:37 -0800

    s

---
+++ LessonOne/sqxw/src/Colass.java
@@ -0,0 +1,3 @@
+public class Colass {
+    iu  fifohu
+}
---
+++ LessonOne/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        //Primitive data types
+        /*
+        int
+        double
+        char
+        boolean
+         */
+
+
+        String firstName = "John";
+        String lastName = "Stewart";
+        String fullName = firstName + lastName ;
+
+
+
+
+
+
+
+    }
+}
