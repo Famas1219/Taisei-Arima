ec3693de386b227d914dfbb2fd950d2f3939292a
Famas1219 <94020078+Famas1219@users.noreply.github.com>
2021-11-12 18:52:06 -0800

    Add files via upload

---
+++ Expressions/src/Expressions.java
@@ -0,0 +1,44 @@
+public class Expressions {
+    public static void main(String args[]){
+
+
+        System.out.println((2 * 5) + 3);
+        System.out.println(10 + 3);
+        System.out.println(13);
+
+        //expression 1
+
+        System.out.println((9 - 4) + 10/2 - (4 - 8)/3);
+        System.out.println((5 * 2) + (9 - 8));
+        System.out.println(8 + 3);
+        System.out.println(11);
+
+        //expression 2
+
+        System.out.println(23%4 + (18 - 3/2));
+        System.out.println((26%3 + 3) + (4 * 2) + (10 - 3));
+        System.out.println(100/5 + 5 - 10/2);
+        System.out.println(20);
+
+        //expression 3
+
+        System.out.println(((8 - 5) + Math.pow(3,4)/5));
+        System.out.println(16 + 3.2 + (2 * 5) - 10);
+        System.out.println(4.2 + (3 * 5) - 4 + 25%7);
+
+        //expression 4
+
+        System.out.println((9 % 4) - 8 + Math.pow(2,3) / 8 + (Math.sqrt(81) / 3));
+        System.out.println(Math.pow(2,4) - (Math.sqrt(16) * (6 - 2)) -3);
+        System.out.println(Math.pow(3,3) - 44/2 -8);
+
+        //expression 5
+
+        System.out.println(((14 / 2 + 3) * Math.sin(45) + 2) - Math.pow(Math.sqrt(25),3));
+        System.out.println(((49 / 7 + 27/9) * Math.sin(45) + (5-3)) - (98+27));
+        System.out.println(((Math.pow(Math.sqrt(81),3) - 719) * Math.sin(45) + (120-118)) - (53+72));
+
+
+    }
+}
+
