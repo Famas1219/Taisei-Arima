bac90bebe305144e678d1d0d15874a00c043898b
Famas1219 <94020078+Famas1219@users.noreply.github.com>
2021-11-15 14:24:56 -0800

    Add files via upload

---
+++ Variable Practice/Computer.java
@@ -0,0 +1,15 @@
+public class Computer{
+    public static void main( String[] args ){
+        int room;
+        double e;
+        String subject ="Computer Science";
+
+        room= 113;
+        e = 2.71828;
+
+        System.out.println( "This is room # " + room);
+        System.out.println( "e is close to " + e);
+        System.out.println( "I am learning a bit about " + subject);
+
+    }
+}
---
+++ Variable Practice/Schedule.java
@@ -0,0 +1,40 @@
+public class Schedule{
+    public static void main( String[] args ) {
+
+
+        //subject
+
+        String subject1 = "Social Studies";
+        String subject2 = "English Learning Center";
+        String subject3 = "Studio Art 2D";
+        String subject4 = "Computer Information System";
+        String subject5 = "Science";
+        String subject6 = "Food Studies";
+        String subject7 = "Composition Transitional";
+        String subject8 = "Pre-Calculus";
+
+        //teacher
+
+        String teacher1 = "Hodgson Jeff";
+        String teacher2 = "Tsai Stephanie";
+        String teacher3 = "Matthews Sarah";
+        String teacher4 = "Zaremba Paul";
+        String teacher5 = "Kim Hailey";
+        String teacher6 = "Walker Ellen";
+        String teacher8 = "Beaton Greg";
+        String topbottom = "+-------------------------------------------------------+";
+
+
+        System.out.println(topbottom);
+        System.out.println("| " + 1 + " |" + "\t\t\t   " + subject1 + "|" + "\t\t\t" + teacher1 + "|");
+        System.out.println("| " + 2 + " |" + "\t  "  +subject2 + "|" + "\t\t  " + teacher2 + "|");
+        System.out.println("| " + 3 + " |" + "\t\t\t\t"  +subject3 + "|" + "\t\t  " + teacher3 + "|");
+        System.out.println("| " + 4 + " |" + " " +  subject4 + "|" + "\t\t\t" + teacher4 + "|");
+        System.out.println("| " + 5 + " |" + "\t\t\t\t\t  " +subject5 + "|" + "\t\t\t  " + teacher5 + "|");
+        System.out.println("| " + 6 + " |" + "\t\t\t\t " + subject6 + "|" + "\t\t\t" + teacher6 + "|");
+        System.out.println("| " + 7 + " |" + "\t " +subject7 + "|" + "\t\t  " + teacher2 + "|");
+        System.out.println("| " + 8 + " |" + "\t\t\t\t " + subject8 + "|" + "\t\t\t " + teacher8 + "|");
+        System.out.println(topbottom);
+
+    }
+    }
